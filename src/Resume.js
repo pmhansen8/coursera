380f2751b199d7ee8e87545380582c85597155d9
pmhansen8 <pmhansen8@gmail.com>
2024-03-17 20:12:58 -0400

    first commit

--- src/Resume.js
+++ src/Resume.js
@@ -3,7 +3,7 @@ import {Box, Heading} from "@chakra-ui/react";
 import Card from "./components/Card";
 import React from "react";
 
-export const Projects = () =>{
+export const Resume = () =>{
     return(
         <FullScreenSection
             backgroundColor="#512DA8"
