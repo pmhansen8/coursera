1cb33632e1fbe3a97c8f983b44b54183e206e2ac
patrick hansen <143661314+pmhansen8@users.noreply.github.com>
2024-06-17 00:22:39 -0700

    Update Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -29,7 +29,7 @@ const Proj = [
     {
         title: "Website",
         description:
-            "Created fully functioning website through coursera course for restaurants.",
+            "Created fully functioning website through coursera course for restaurant.",
         imageSrc: photo3,
         url: "https://github.com/pmhansen8/react-project",
     },
